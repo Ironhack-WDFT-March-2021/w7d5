f2b5478548c74c5eb088db1c34aae050b1b55e43
Jan Redmann <janredmann1@gmail.com>
2021-05-07 18:02:28 +0200

    add realtime

--- realtime/server.js
+++ realtime/server.js
@@ -3,7 +3,7 @@ const socket = require('socket.io');
 const cors = require('cors');
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
